a6e2d439a718e6f46f4c7891cb731c4dcc10908c
Michael <Cholinesterasa@gmail.com>
2021-10-11 15:54:54 +0300

    Dz_3 с другого компа

--- Ignatov_Mikhail_dz_3/Exercize_5.py
+++ Ignatov_Mikhail_dz_3/Exercize_5.py
@@ -1,11 +1,14 @@
 import random
-#
+# вообще не разобрался с аргументами =(
 nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 
-def get_jokes(n):
+def get_jokes(n: int):
+    """домашку доделать не успел, + наставник на помощь не пришёл,
+    скинул как есть.
+        :param n:int надо ввести цифру"""
     result = []
     for i in range(n):
         result.append(f'{random.choice(nouns)} {random.choice(adverbs)} {random.choice(adjectives)}')
