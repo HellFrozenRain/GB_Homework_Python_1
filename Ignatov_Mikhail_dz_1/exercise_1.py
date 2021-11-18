201bf9364ba4c76450448b909bcaec0e5c57a178
Michael <Cholinesterasa@gmail.com>
2021-11-19 01:04:00 +0300

    dz_1 again

--- Ignatov_Mikhail_dz_1/exercise_1.py
+++ Ignatov_Mikhail_dz_1/exercise_1.py
@@ -1,20 +1,34 @@
-duration = int(input('time ?'))
+''' 1. Реализовать вывод информации о промежутке времени
+в зависимости от его продолжительности duration в секундах:
+до минуты: <s> сек;
+до часа: <m> мин <s> сек;
+до суток: <h> час <m> мин <s> сек;
+* в остальных случаях: <d> дн <h> час <m> мин <s> сек.
+        Примеры:
+
+duration = 53
+53 сек
+duration = 153
+2 мин 33 сек
+duration = 4153
+1 час 9 мин 13 сек
+duration = 400153
+4 дн 15 час 9 мин 13 сек
+'''
+duration = int(input('Введите продолжительность времени'))
+days = duration // 86400 # дни - целочисленное деление на 24*3600
+hours = (duration % 86400) // 3600 # часы - целочисленное деление на 3600
+minutes = (duration % 3600) // 60 # минуты - целочисленное деление на 60
+seconds = duration % 60 # секунды - остаток от еления на 60
+
 if duration < 60:
-    print("{} seconds".format(duration))
-elif duration >= 60 and duration < 3600:
-    minutes = duration // 60
-    sec = duration % 60
-    print("{} minutes {} seconds".format(minutes, sec))
+    print(f'{seconds} сек.')
+elif duration >= 60 and duration  < 3600:
+    print(f'{minutes} мин. {seconds} сек.')
 elif duration >= 3600 and duration < 86400:
-    hours = duration // 3600
-    minutes = duration % 3600 // 60
-    sec = duration % 3600 % 60
-    print("{} hours {} minutes {} seconds".format(hours, minutes, sec))
-elif duration >= 86400 and duration < 31536000:
-     days = duration // 86400
-     hours = duration % 24
-     minutes = duration % 3600 // 60
-     sec = duration % 3600 % 60
-     print("{} days {} hours {} minutes {} seconds".format(days, hours, minutes, sec))
+    print(f'{hours} час. {minutes} мин. {seconds} сек.')
+elif duration >= 86400:
+    print(f'{days} дн. {hours} час. {minutes} мин. {seconds} сек.')
+
 else:
-    print('see you next time')
+    print('end')
